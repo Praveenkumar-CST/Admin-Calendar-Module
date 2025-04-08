6429505809b676ab7f04335276a21c63219cd8e1
Praveenkumar <praveenkumar@Praveenkumars-MacBook-Air.local>
2025-04-08 09:51:14 +0530

    first commit

--- Admin/obj/Debug/net9.0/Admin.AssemblyInfo.cs
+++ Admin/obj/Debug/net9.0/Admin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966c98098aea99cbf822d503e5379292b480e6f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc854a24c94a2b27a14800b335d47ede5930739")]
 [assembly: System.Reflection.AssemblyProductAttribute("Admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
